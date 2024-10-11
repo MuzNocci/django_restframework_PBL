f9f2e7c3a270944d8a0e95f483c221fe12bf00f0
Müller Nocciolli <muller.nocciolli@gmail.com>
2024-10-11 13:11:05 -0300

    first commit

---
+++ app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from app.views import TodoListAndCreate, TodoListAndUpdateAndDelete
+
+
+
+urlpatterns = [
+
+    path('', TodoListAndCreate.as_view()),
+    path('<int:pk>/', TodoListAndUpdateAndDelete.as_view()),
+
+]
